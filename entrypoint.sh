670025b0b422e97313a670a9b972e5febd75daa7
Arvid Gräns <79848215+ArGr1@users.noreply.github.com>
2021-03-24 11:15:03 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ if [ "${1:-}" = 'RUN' ]; then
       echo "1st parameter is: ${1:-}"
 
       if [[ "$file" == *"${2-}"* ]]; then
-        mv "$file" "index.${filename##*.}"
+        mv "$file" "index.${file##*.}"
         echo "Inside if statement with parameter: ${2-}"
       fi
 
