b8b24fa633b2eb1acca27c27c6fad75408b3dd62
Sindre Nistad <snis@equinor.com>
2021-06-29 16:48:47 +0200

    fix: Use same output directory

--- run-locally.sh
+++ run-locally.sh
@@ -9,6 +9,6 @@ echo "${PWD}"
 docker run \
     --rm \
     -it \
-    -v "${PWD}/dist":/home/pptruser/dist/ \
+    -v "${PWD}/dist":/github/workspace/dist/ \
     -v "${PWD}/docs":/home/pptruser/docs/ \
     asciidoctor-action
